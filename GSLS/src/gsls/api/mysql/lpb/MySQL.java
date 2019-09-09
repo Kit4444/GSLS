5f93be890bf321e59aa8359251f82afcf0de01d8
MauriceLPs <maurice.hoefner@gmail.com>
2019-09-10 01:26:21 +0200

    Implemented MySQL-API from Letsplaybar

---
+++ GSLS/src/gsls/api/mysql/lpb/MySQL.java
@@ -0,0 +1,27 @@
+package gsls.api.mysql.lpb;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQL extends SQLImpl{
+	
+	private String host;
+	private int port;
+	private String db;
+	private String user;
+	private String pw;
+	
+	public MySQL(String host, int port, String db, String user, String pw) {
+		this.host = host;
+		this.port = port;
+		this.db = db;
+		this.user = user;
+		this.pw = pw;
+	}
+	
+	@Override
+	public void connect() throws SQLException{
+		con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db + "?autoReconnect=true&serverTimezone=UTC", user, pw);
+	}
+
+}
