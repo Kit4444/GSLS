1e4d8484166bd825db0b1746ae080fc0eae0415d
Maurice <Maurice@DESKTOP-457TCCI>
2020-03-21 00:31:11 +0100

    Added Togglegroup

--- GSLS/src/gsls/system/cmd/LogCMD.java
+++ GSLS/src/gsls/system/cmd/LogCMD.java
@@ -1,5 +1,7 @@
 package gsls.system.cmd;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -9,7 +11,11 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import gsls.api.mysql.lb.MySQL;
+import gsls.system.LanguageHandler;
 import gsls.system.Main;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class LogCMD implements CommandExecutor{
 	
@@ -24,32 +30,63 @@ public class LogCMD implements CommandExecutor{
 					HashMap<String, Object> log = new HashMap<>();
 					log.put("Name", p.getUniqueId().toString().replaceAll("-", ""));
 					try {
-						Main.mysql.update("loginsys", "loggedin", true, log);
-						p.sendMessage(Main.prefix + "§7Successfully logged in.");
+						if(logStat(p) == true) {
+							LanguageHandler.sendMSGReady(p, "cmd.login.already");
+						}else {
+							Main.mysql.update("loginsys", "loggedin", true, log);
+							LanguageHandler.sendMSGReady(p, "cmd.login.success");
+						}
 					}catch (SQLException e) {
-						p.sendMessage(Main.unavailable);
-						e.printStackTrace();
 					}
 				}else {
-					p.sendMessage(Main.noperm("mlps.canBan"));
+					LanguageHandler.noperm(p);
 				}
 			}else if(cmd.getName().equalsIgnoreCase("logout")) {
 				if(p.hasPermission("mlps.canBan")) {
 					HashMap<String, Object> log = new HashMap<>();
 					log.put("Name", p.getUniqueId().toString().replaceAll("-", ""));
 					try {
-						Main.mysql.update("loginsys", "loggedin", false, log);
-						p.sendMessage(Main.prefix + "§7Successfully logged out.");
+						if(!logStat(p) == true) {
+							LanguageHandler.sendMSGReady(p, "cmd.logout.already");
+						}else {
+							Main.mysql.update("loginsys", "loggedin", false, log);
+							LanguageHandler.sendMSGReady(p, "cmd.logout.success");
+						}
 					}catch (SQLException e) {
-						p.sendMessage(Main.unavailable);
-						e.printStackTrace();
 					}
 				}else {
-					p.sendMessage(Main.noperm("mlps.canBan"));
+					LanguageHandler.noperm(p);	
+				}
+			}else if(cmd.getName().equalsIgnoreCase("tg")) {
+				PermissionUser po = PermissionsEx.getUser(p);
+				if(po.inGroup("Patron") || po.inGroup("Beta") || po.inGroup("Mod") || po.inGroup("Support") || po.inGroup("Builder") || po.inGroup("VIPpls") || po.inGroup("NitroBooster") || po.inGroup("Friend") || po.inGroup("Premiumpls")) {
+					HashMap<String, Object> log = new HashMap<>();
+					log.put("Name", p.getUniqueId().toString().replaceAll("-", ""));
+					try {
+						if(logStat(p) == true) {
+							Main.mysql.update("loginsys", "loggedin", false, log);
+							LanguageHandler.sendMSGReady(p, "cmd.tg.hide");
+						}else {
+							Main.mysql.update("loginsys", "loggedin", true, log);
+							LanguageHandler.sendMSGReady(p, "cmd.tg.visible");
+						}
+					}catch (SQLException e) {
+						LanguageHandler.unavailable(p);
+					}
+				}else {
+					LanguageHandler.noperm(p);
 				}
 			}
 		}
 		return true;
 	}
-
+	
+	private boolean logStat(Player p) throws SQLException {
+		PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT * FROM loginsys WHERE Name = ?");
+		ps.setString(1, p.getUniqueId().toString().replaceAll("-", ""));
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		boolean boo = rs.getBoolean("loggedin");
+		return boo;
+	}
 }
