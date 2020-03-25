7a5be81de86ee3799c529c929c3b2a8af6aa61fb
Maurice <Maurice@DESKTOP-457TCCI>
2020-03-25 18:01:51 +0100

    Added necessary methods for Navigator

--- GSLS/src/gsls/api/mc/ItemsAPI.java
+++ GSLS/src/gsls/api/mc/ItemsAPI.java
@@ -1,5 +1,7 @@
 package gsls.api.mc;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -13,5 +15,40 @@ public class ItemsAPI {
 		item.setItemMeta(mitem);
 		return item;
 	}
-
+	
+	public static ItemStack onlineItem(Material mat, int avg, int subid, String dpname, int online) {
+		ArrayList<String> lore = new ArrayList<>();
+		ItemStack item = new ItemStack(mat, avg, (short)subid);
+		ItemMeta mitem = item.getItemMeta();
+		lore.add("§aOnline§7: " + online);
+		mitem.setLore(lore);
+		mitem.setDisplayName(dpname);
+		item.setItemMeta(mitem);
+		return item;
+	}
+	
+	public static ItemStack l2Item(Material mat, int avg, int subid, String dpname, String lore1, String lore2) {
+		ArrayList<String> lore = new ArrayList<>();
+		ItemStack item = new ItemStack(mat, avg, (short)subid);
+		ItemMeta mitem = item.getItemMeta();
+		lore.add(lore1);
+		lore.add(lore2);
+		mitem.setLore(lore);
+		mitem.setDisplayName(dpname);
+		item.setItemMeta(mitem);
+		return item;
+	}
+	
+	public static ItemStack l2onItem(Material mat, int avg, int subid, String dpname, String lore2, String lore3, int online) {
+		ArrayList<String> lore = new ArrayList<String>();
+	    ItemStack item = new ItemStack(mat, avg, (short)subid);
+	    ItemMeta mitem = item.getItemMeta();
+	    lore.add("§aOnline§7: " + online);
+	    lore.add(lore2);
+	    lore.add(lore3);
+	    mitem.setLore(lore);
+	    mitem.setDisplayName(dpname);
+	    item.setItemMeta(mitem);
+	    return item;
+	}
 }
